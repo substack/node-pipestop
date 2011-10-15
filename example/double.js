08a899fbe4f44a5396c3fe0b82d606f3f64f7516
James Halliday <mail@substack.net>
2011-10-14 23:33:13 -0700

    more examples

---
+++ example/double.js
@@ -0,0 +1,33 @@
+var divert = require('../');
+var EventEmitter = require('events').EventEmitter;
+
+var em = new EventEmitter;
+var pipe = divert(em);
+
+var ix = 0;
+setInterval(function () {
+    em.emit('data', ix++);
+}, 500);
+
+var s0 = fakeStream(0);
+var stop0 = pipe(s0);
+
+var s1 = fakeStream(1);
+var stop1 = pipe(s1);
+
+setTimeout(function () {
+    stop0();
+}, 1001);
+
+setTimeout(function () {
+    stop1();
+}, 3001);
+
+function fakeStream (id) {
+    var s = new EventEmitter;
+    s.writable = true;
+    s.write = function (x) {
+        console.log(id + ' wrote ' + x);
+    };
+    return s;
+}
---
+++ example/single.js
@@ -0,0 +1,26 @@
+var divert = require('../');
+var EventEmitter = require('events').EventEmitter;
+
+var em = new EventEmitter;
+var pipe = divert(em);
+
+var ix = 0;
+setInterval(function () {
+    em.emit('data', ix++);
+}, 500);
+
+var s = fakeStream(1);
+var stop = pipe(s);
+
+setTimeout(function () {
+    stop();
+}, 3001);
+
+function fakeStream () {
+    var s = new EventEmitter;
+    s.writable = true;
+    s.write = function (x) {
+        console.log('wrote ' + x);
+    };
+    return s;
+}
